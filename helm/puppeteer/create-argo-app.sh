0a2ba78d3df239d3581c86844ce3c52dc88d06df
Alfred Dobradi <alfreddobradi@gmail.com>
2024-03-02 18:39:37 +0000

    chore: Bump puppeteer version in create argo app script

--- helm/puppeteer/create-argo-app.sh
+++ helm/puppeteer/create-argo-app.sh
@@ -3,6 +3,6 @@ argocd app create genesis \
     --project genesis-avalon \
     --repo registry.0x42.in/genesis-avalon/helm \
     --helm-chart puppeteer \
-    --revision 0.1.1 \
+    --revision 0.1.2 \
     --dest-server "https://kubernetes.default.svc" \
     --dest-namespace genesis-avalon-dev
