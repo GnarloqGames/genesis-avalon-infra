7b7f3751ea13686d375c01559eac64d093fdebf8
Alfred Dobradi <alfreddobradi@gmail.com>
2024-03-03 10:42:59 +0000

    fix: Use git repo instead of helm chart for controller app in Argo

--- helm/puppeteer/create-argo-app.sh
+++ helm/puppeteer/create-argo-app.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 argocd app create genesis \
     --project genesis-avalon \
-    --repo registry.0x42.in/genesis-avalon/helm \
-    --helm-chart puppeteer \
-    --revision 0.1.2 \
+    --repo git@github.com:GnarloqGames/genesis-avalon-infra.git \
+    --path helm/puppeteer \
+    --revision main \
     --dest-server "https://kubernetes.default.svc" \
     --dest-namespace genesis-avalon-dev
